fa04fc706c5daf21f99c736d3ee1ef4e67cbcfe8
zishan <zishanza436@gmail.com>
2026-05-09 01:00:35 +0530

    CNN prediction added to the model

--- Projects/Hand Written Recognition/web/.ipynb_checkpoints/app-checkpoint.py
+++ Projects/Hand Written Recognition/web/.ipynb_checkpoints/app-checkpoint.py
@@ -2,27 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
-from tensorflow import keras
-import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
-model_ml = joblib.load("./XGBoost_gpu_based.pkl")
-model_cnn = keras.models.load_model("mnist_cnn_model.h5")
+model = joblib.load("./XGBoost_gpu_based.pkl")
 
+# Load your trained model
+# with open("model.pkl", "rb") as f:
+#     model = pickle.load(f)
 
 @app.route("/guess", methods=["POST"])
 def guess():
    data = request.get_json()
    df = pd.DataFrame([data])
-   result_ml = model_ml.predict(df)
-   pixels = [data[f'pixel_{i}'] for i in range(784)]
-   img = np.array(pixels, dtype=np.float32)
-   img = img.reshape(1, 28, 28, 1)
-   pred = model_cnn.predict(img)
-   result_cnn = np.argmax(pred)
-   return jsonify({"prediction_ml":int(result_ml[0]),"prediction_cnn":int(result_cnn)})
+   result = model.predict(df)
+   return jsonify({"prediction":int(result[0])})
 
 @app.route("/", methods=["GET"])
 def landing():
