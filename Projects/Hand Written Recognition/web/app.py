4572e8b04c5590440a2c94e5c3a31e9458c0d4b9
zishan <zishanza436@gmail.com>
2026-05-09 17:36:18 +0530

    cnn model donwgraded

--- Projects/Hand Written Recognition/web/app.py
+++ Projects/Hand Written Recognition/web/app.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
-from tensorflow import keras
+# from tensorflow import keras
 import numpy as np
 
 app = Flask(__name__)
 CORS(app)
 
 model_ml = joblib.load("./XGBoost_gpu_based.pkl")
-model_cnn = keras.models.load_model("mnist_cnn_model.h5")
+# model_cnn = keras.models.load_model("mnist_cnn_model.h5")
 
 
 @app.route("/guess-ml", methods=["POST"])
@@ -19,15 +19,15 @@ def guess_ml():
    result_ml = model_ml.predict(df)
    return jsonify({"prediction_ml":int(result_ml[0])})
 
-@app.route("/guess-cnn",methods=["POST"])
-def guess_cnn():
-   data = request.get_json()
-   pixels = [data[f'pixel_{i}'] for i in range(784)]
-   img = np.array(pixels, dtype=np.float32)
-   img = img.reshape(1, 28, 28, 1)
-   pred = model_cnn.predict(img)
-   result_cnn = np.argmax(pred)
-   return jsonify({"prediction_cnn":int(result_cnn)})
+# @app.route("/guess-cnn",methods=["POST"])
+# def guess_cnn():
+#    data = request.get_json()
+#    pixels = [data[f'pixel_{i}'] for i in range(784)]
+#    img = np.array(pixels, dtype=np.float32)
+#    img = img.reshape(1, 28, 28, 1)
+#    pred = model_cnn.predict(img)
+#    result_cnn = np.argmax(pred)
+#    return jsonify({"prediction_cnn":int(result_cnn)})
 
 @app.route("/", methods=["GET"])
 def landing():
